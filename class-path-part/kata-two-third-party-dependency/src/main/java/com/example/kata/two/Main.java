17506053b828c3073a003529c34eae99f8cb4bd7
krenMaksim <krenmaba@gmail.com>
2024-04-27 07:09:39 +0300

    updated class-path-part/kata-two-third-party-dependency/src/main/java/com/example/kata/two/Main.java

--- class-path-part/kata-two-third-party-dependency/src/main/java/com/example/kata/two/Main.java
+++ class-path-part/kata-two-third-party-dependency/src/main/java/com/example/kata/two/Main.java
@@ -1,10 +1,10 @@
 package com.example.kata.two;
 
-import com.google.common.base.Optional;
+import com.google.common.base.Strings;
 
 class Main {
 
     public static void main(String[] args) {
-        System.out.println("Optional.absent(): " + Optional.absent());
+        System.out.println(Strings.lenientFormat("%s %s", "Hello", "World!"));
     }
 }
